9f8887e7402c97c4823678719c24c3407ad84f43
Samuel Afonso Gomes <samuel.gomes.afonso@gmail.com>
2020-09-22 12:48:22 -0300

    lista de instituicoes pelos bairros
    tambem comentei pra facilitar o entendimento

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
 var inst = [];
+var list_bairro = []
 $.get("data.json",
     function (data) {
-        console.log(data.length);
-        for(var i = 0; i < data.length; i++){
+        /**transforma o arquivo .json em um array */
+        for(let i = 0; i < data.length; i++){
             inst.push(data[i]);
-            console.log(data[i].nome);
-            console.log(data[i].numero);
-            console.log(data[i].bairro);
         }
-        console.log(inst);;
+        
+        /**organiza o array de instituicoes numa lista por bairros
+         * Teste de cada instituicao pra ver se ja existe uma lista pro seu bairro
+         * Caso haja ela e adicionada nessa lista, senao e criada uma nova lista
+         */
+        let match = false; //var para o controle de checagem
+        let matchNum = null;
+        for(let i = 0; i < inst.length; i++){
+            for (let j = 0; j < list_bairro.length; j++) {
+                if (list_bairro[j].bairro == inst[i].bairro) {
+                    match = true;
+                    matchNum = j;
+                    break;
+                }
+            }
+            if(match){
+                match = false;
+                list_bairro[matchNum].list.push(inst[i]);
+            }else{
+                /**ESTRUTURA
+                 * [{
+                 *      bairro : "nome do bairro",
+                 *      list : [
+                 *          {instituicao 01},
+                 *          {instituicao 02},
+                 *          etc...
+                 *      ]
+                 * },
+                 * {
+                 *    bairro : "nome do outro bairro",
+                 *      list : [
+                 *          {instituicao 01},
+                 *          {instituicao 02},
+                 *          etc...
+                 *},
+                 *
+                 * etc...
+                 *        
+                 *}]
+                 */
+                list_bairro.push({bairro : inst[i].bairro, list : [inst[i]]});
+            }
+        }
     }
 );
