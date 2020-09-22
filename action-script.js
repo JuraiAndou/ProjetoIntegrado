080fddab74952901214ef415c2fb852628f9ab64
Bruno Oliveira <brunno.os159@gmail.com>
2020-09-22 17:23:31 -0300

    Função de abrir Info do Local ao clicar no +

--- action-script.js
+++ action-script.js
@@ -1,12 +1,4 @@
-﻿function AddClickEvent() {
-    for (var i = 0; i < document.getElementsByClassName("bairro-header").length; i++) {
-        document.getElementsByClassName("bairro-header")[i].addEventListener("click", toggleBairroList);
-    }
-}
-
-AddClickEvent();
-
-function toggleBairroList(){
+﻿function toggleBairroList(){
     if (this.getAttribute("open") != "true") {
         this.setAttribute("open", "true");
         this.classList.add("bairro-header-selected");
@@ -29,7 +21,7 @@ document.getElementById("filter-button").addEventListener("click", function () {
     var filterTab = document.getElementById("filter-tab");
 
     filterTabBg.style.display = "block";
-    filterTabBg.style.animationName = "openFilterTabBg";
+    filterTabBg.style.animationName = "showBg";
     filterTab.style.animationName = "openFilterTab";
 
 });
@@ -37,7 +29,40 @@ document.getElementById("filter-button").addEventListener("click", function () {
 document.getElementById("close-filter-tab").addEventListener("click", function () {
     var filterTabBg = document.getElementById("filter-tab-bg")
     var filterTab = document.getElementById("filter-tab");
+
     var id = setTimeout(function (){ filterTabBg.style.display = "none" }, 500);
-    filterTabBg.style.animationName = "closeFilterTabBg";
+    filterTabBg.style.animationName = "hideBg";
     filterTab.style.animationName = "closeFilterTab";
 });
+
+function openLocalInfo() {
+    var localInfoBg = document.getElementById("local-block-bg")
+    var localInfo = document.getElementById("local-block");
+
+    localInfoBg.style.display = "block";
+    localInfoBg.style.animationName = "showBg";
+    localInfo.style.animationName = "openLocalInfo";
+}
+
+function closeLocalInfo() {
+    var localInfoBg = document.getElementById("local-block-bg")
+    var localInfo = document.getElementById("local-block");
+
+    var id = setTimeout(function (){ localInfoBg.style.display = "none" }, 500);
+    localInfoBg.style.animationName = "hideBg";
+    localInfo.style.animationName = "closeLocalInfo";
+}
+
+function AddClickEvent() {
+    for (var i = 0; i < document.getElementsByClassName("bairro-header").length; i++) {
+        document.getElementsByClassName("bairro-header")[i].addEventListener("click", toggleBairroList);
+    }
+
+    for (var i = 0; i < document.getElementsByClassName("local-ref-info").length; i++) {
+        document.getElementsByClassName("local-ref-info")[i].addEventListener("click", openLocalInfo);
+    }
+
+    document.getElementById("close-local-block").addEventListener("click", closeLocalInfo);
+}
+
+window.onLoad = AddClickEvent();
